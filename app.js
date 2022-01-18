8328cc297f726b176948ef3db8e2155a2d15afe0
Creigh Battle <creighbattle@Creighs-MacBook-Pro.local>
2022-01-17 22:28:04 -0500

    db changes

--- app.js
+++ app.js
@@ -9,26 +9,49 @@ const pool = new Pool({
     port: 5432
 })
 
-pool.connect().then(() => console.log('connected')).catch((e) => console.log(e));
 
 
-pool.query("create table testing(id bigserial primary key, name varchar(20)").then(() => {
-    pool.query("insert into testing (name) values ('Creigh')")
-})
+// pool.query("create table testing(id bigserial primary key, name varchar(20)").then(() => {
+//     pool.query("insert into testing (name) values ('Creigh')")
+// })
 
 
-app.get('/', (req, res) => {
-    res.send("Welcome to the home page baby");
+app.get('/', async (req, res) => {
+    try {
+        const val = await pool.query('select * from fun');
+        console.log(val.rows[0]);
+        res.send("Welcome to the home page baby");
+    } catch (error) {
+        await pool.query("create table fun(id bigserial primary key, name varchar(20))")
+        console.log(error.message)
+        res.send("We are not connected");
+    }
 });
 
+app.get('/add', async (req, res) => {
+    try {
+        const val = await pool.query("insert into fun (name) values ('creigh')");
+        res.send("Added");
+    } catch (error) {
+        res.send(error.message);
+    }
+})
+
 app.get('/db', async (req, res) => {
-    const shop = await pool.query(
-      "select name from testing"
-    );
+    // const shop = await pool.query(
+    //   "select name from testing"
+    // );
 
-    res.send(shop.rows[0])
+    // res.send(shop.rows[0])
 
-    console.log(shop.rows[0]);
+    // console.log(shop.rows[0]);
+
+    try {
+        const val = await pool.query('select * from fun');
+        res.send(val.rows);
+    } catch (error) {
+        res.send(error.message);
+    }
 })
 
 const port = process.env.PORT || 3000;
