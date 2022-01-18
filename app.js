99814aee3ea018df8bc463037ae3272e896e1c31
Creigh Battle <creighbattle@Creighs-MacBook-Pro.local>
2022-01-17 21:31:29 -0500

    database connection

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const {Pool} = require('pg');
 const app = express();
+const pool = new Pool({
+    user: "postgres",
+    password: "postgres",
+    host: process.env.RDS_HOSTNAME || "localhost",
+    database: "postgres",
+    port: 5432
+})
+
+pool.connect().then(() => console.log('connected')).catch((e) => console.log(e));
+
 
 app.get('/', (req, res) => {
     res.send("Welcome to the home page baby");
 });
+
+app.get('/db', async (req, res) => {
+    const shop = await pool.query(
+      "select name from testing"
+    );
+
+    res.send(shop.rows[0])
+
+    console.log(shop.rows[0]);
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
